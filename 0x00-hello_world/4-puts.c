1574aada2f745d0afd47a343defa7a66e6b45014
ckchesire <christianck.chesire@gmail.com>
2024-06-20 17:48:19 -0700

    Fix update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* Display message. */
+/*
+ * Display message.
+ * Code prints out string.
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
