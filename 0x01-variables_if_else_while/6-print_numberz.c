232f6ae3394fc64252df56102f18a9464d3ca21f
ckchesire <christianck.chesire@gmail.com>
2024-06-23 10:23:06 +0000

    Using ASCII format to print 48-57 representation.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,17 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description :  Print single digit characters 0-9.
+ * Description :  Print single digit characters using ASCII 48-57.
  * Return: Always '0' (success)
  */
 int main(void)
 {
-	char n;
+	int n;
 
-	n = '0';
-	while (n <= '9')
+	n = 0;
+	while (n <= 9)
 	{
-		putchar( n);
+		putchar('0' + n);
 		n++;
 	}
 	putchar('\n');
