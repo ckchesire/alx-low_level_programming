eb15b0b62785cd6a312ed0279500b1f2751c3c65
ckchesire <christianck.chesire@gmail.com>
2024-06-29 01:26:17 +0000

    Diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - function that draws a diagonal line
+ * @n: integer for number of times \ should be printed
+ * Return: diagonal line
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
