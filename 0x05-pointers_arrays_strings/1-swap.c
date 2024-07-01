d6ccaf48c3000b0fd4a8c7deb8d076d31b49c705
ckchesire <christianck.chesire@gmail.com>
2024-07-01 22:39:41 +0000

    Swap the values of two integers.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: First integer
+ * @b: Second integer
+ * @Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}	
