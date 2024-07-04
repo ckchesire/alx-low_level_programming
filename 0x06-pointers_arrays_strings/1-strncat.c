4011b71d8db0ef024c942bf5b533d3351469b88a
ckchesire <christianck.chesire@gmail.com>
2024-07-04 09:16:21 +0000

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenats two strings
+ * @dest: Destination variable to copy string
+ * @src: Source variable to copy string from
+ * Return: returns final concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	char *ptr;
+
+	ptr = dest;
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	
+	if (n == 1)
+	{
+		*ptr++ = *src;
+	}
+	else 
+	{
+		for (i = 0; i <= n; i++)
+		{
+			*ptr = *src++;
+		}
+	}
+	return dest;
+}
