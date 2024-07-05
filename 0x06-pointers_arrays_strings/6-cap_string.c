efa67fb2893a88d75b5fea055e9fe3fff060a97b
ckchesire <christianck.chesire@gmail.com>
2024-07-05 01:59:45 +0000

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * cap_string: function that capitalizes all words of string
+ * cap_string - function that capitalizes all words of string
  * @str: string input
  * Return: Caplitalize string output
  */
@@ -10,15 +10,14 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		while(!(str[i] >='a' && str[i] <= 'z'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
 			i++;
 		}
-			
-		if (str[i - 1] == ' '|| str[i - 1] == '\t' || str[i - 1] == '\n'||
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
 				str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-				str[i - 1] == '!' || str[i - 1] == '?'|| str[i - 1] == '"'|| 
-				str[i - 1] == '(' || str[i - 1] == ')'|| str[i - 1] =='{'||
+				str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+				str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
 				str[i - 1] == '}' || i == 0)
 		{
 			str[i] -= 32;
