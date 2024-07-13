d17d7f1a8f41819e6f07408322442724d3745ce3
ckchesire <christianck.chesire@gmail.com>
2024-07-13 11:45:50 +0000

    Program that adds +ve numbers, 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that adds positive numbers
+ * @argc: count arguments
+ * @argv: arguments array
+ * Return: '0' success
+ */
+int main(int argc, char **argv)
+{
+	int i = 0, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if(atoi(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return(1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
