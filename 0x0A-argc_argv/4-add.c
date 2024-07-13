09c5026f4e2f32b9427f20defdfa20741da5a573
ckchesire <christianck.chesire@gmail.com>
2024-07-13 11:46:38 +0000

    Program that adds +ve numbers, 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,14 +12,14 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		if(atoi(argv[i]))
+		if (atoi(argv[i]))
 		{
 			sum += atoi(argv[i]);
 		}
 		else
 		{
 			printf("Error\n");
-			return(1);
+			return (1);
 		}
 	}
 	printf("%d\n", sum);
