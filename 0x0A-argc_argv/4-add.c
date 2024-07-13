bafeb1f6125a9fde4e1d666447f9c09586491876
ckchesire <christianck.chesire@gmail.com>
2024-07-13 11:59:06 +0000

    Updated 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,26 +9,23 @@
 int main(int argc, char **argv)
 {
 	int i = 0, sum = 0;
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (atoi(argv[i]))
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-	printf("%d\n", sum);
-	return (0);
-	}
-	else
+	if(argc <= 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
