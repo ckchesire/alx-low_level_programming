e122a9fc414ebc2bb7aa0dd555be708c87da095c
ckchesire <christianck.chesire@gmail.com>
2024-07-17 02:55:05 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,37 +12,22 @@ char *str_concat(char *s1, char *s2)
 	int j, cnt, len1, len2;
 	char *s;
 
-	j = 0;
-	cnt = 0;
-	len1 = 0;
-	len2 = 0;
+	j = 0, cnt = 0, len1 = 0, len2 = 0;
 	if (s1 == NULL)
-	{
-		s1[1] = '\0';
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2[1] = '\0';
-	}
+		s2 = "";
 	while (s1[len1])
-	{
 		len1++;
-	}
 	while (s2[len2])
-	{
 		len2++;
-	}
 	s = malloc((len1 + len2 + 1) * sizeof(char));
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	while(cnt < (len1 + len2))
 	{
 		if (cnt	<= len1)
-		{
 			s[cnt] = s1[cnt];
-		}
 		if (cnt >= len1)
 		{
 			s[cnt] = s2[j];
