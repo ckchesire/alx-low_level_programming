d6841d595e1a77a424d1a8febfc67daa423cf277
ckchesire <christianck.chesire@gmail.com>
2024-07-23 04:36:44 +0300

    Define struct, 0.Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - template for 'dog'
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
