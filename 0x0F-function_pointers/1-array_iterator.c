5173f465f242d095221ef7813a9b2f94fee99470
ckchesire <christianck.chesire@gmail.com>
2024-07-25 00:57:30 +0300

    Update:Add function pointer array_iterator that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
+	if (array == NULL || size == 0 || action == NULL)
+		return;
+
 	for (; i < size; i++)
 	{
 		action(array[i]);
 	}
+
 }
