c15cf9320f775e55c6aaae316f9878533b6c2f62
ckchesire <christianck.chesire@gmail.com>
2024-07-25 23:21:17 +0300

    Add variadic function print_numbers to print numbers formated by separator.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,10 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (separator == NULL || n == 0)
 		return;
 
-	for(i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if( i == (n - 1))
+		if (i == (n - 1))
 		{
 			printf("\n");
 			va_end(ap);
