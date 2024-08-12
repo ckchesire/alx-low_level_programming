6111136bcfa709f4be4ce39731805cdfc06bdfc4
ckchesire <christianck.chesire@gmail.com>
2024-08-13 02:21:27 +0300

    Update: New added a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,31 +9,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int file_p;
-        char *buffer;
-        ssize_t len, output_len;
+	int file_p;
+	char *buffer;
+	ssize_t len, output_len;
 
-        if (filename == NULL)
-                return (0);
-        file_p = open(filename, O_RDONLY);
-        if (file_p == -1)
-                return (0);
-        buffer = malloc(sizeof(char) * letters);
-        if (buffer == NULL)
-        {
-                close(file_p);
-                return (0);
-        }
-        len = read(file_p, buffer, letters);
-        close(file_p);
-        if (len == -1)
-        {
-                free(buffer);
-                return (0);
-        }
-        output_len = write(STDOUT_FILENO, buffer, len);
-        free(buffer);
-        if (len != output_len)
-                return (0);
-        return (output_len);
+	if (filename == NULL)
+		return (0);
+	file_p = open(filename, O_RDONLY);
+	if (file_p == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_p);
+		return (0);
+	}
+	len = read(file_p, buffer, letters);
+	close(file_p);
+	if (len == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	output_len = write(STDOUT_FILENO, buffer, len);
+	free(buffer);
+	if (len != output_len)
+		return (0);
+	return (output_len);
 }
