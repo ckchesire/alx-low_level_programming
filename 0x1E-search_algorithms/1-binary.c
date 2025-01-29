509c8a8d767dee2096499b624cd83ac70949b273
Christian Chesire <christianck.chesire@gmail.com>
2025-01-29 15:23:01 -0800

    Updated 1-binary.c to align with betty.

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,33 +10,35 @@
  *
  * Return: The index of the value being searched
  */
- 
- int binary_search(int *array, size_t size, int value)
- {
-    size_t low = 0, middle, i;
-    size_t high = size - 1;
-    int middle_value;
 
-    while (low <= high) {
-        middle = low + (high - low) / 2;
-        middle_value = array[middle];
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low = 0, middle, i;
+	size_t high = size - 1;
+	int middle_value;
 
-        printf("Searching in array: ");
-        for (i = low; i <= high; i++)
-        {
-            printf("%d", array[i]);
-            if (i < high) {
-                printf(", ");
-            }
-        }
-        printf("\n");
+	while (low <= high)
+	{
+		middle = low + (high - low) / 2;
+		middle_value = array[middle];
 
-        if (middle_value < value)
-            low = middle + 1;
-        else if (middle_value > value)
-            high = middle - 1;
-        else
-            return (middle);
-    }
-    return (-1);
- }
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+
+		if (middle_value < value)
+			low = middle + 1;
+		else if (middle_value > value)
+			high = middle - 1;
+		else
+			return (middle);
+	}
+	return (-1);
+}
